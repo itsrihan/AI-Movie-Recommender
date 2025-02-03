bd65246768c7180518307e604990b26448e7c9ad
ri 彡 <130314965+itsrihan@users.noreply.github.com>
2025-02-03 23:02:23 +0400

    Update popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,6 +1,6 @@
 document.getElementById('prompt_button').addEventListener('click', async () => {
 const prompt_text = document.getElementById('prompt_text').value;
-const response = await fetch('http://127.0.0.1:5000/recommend', {
+const response = await fetch('Flask_API_URL', {
     method: 'POST', 
     headers: {
         'content-type': 'application/json'
